326cf5a0a4bc1bdeb66c230fb1fe7a0f1c0dddbf
Suzanne Harrison <harrison_family@hotmail.com>
2016-04-20 20:25:31 -0700

    Added the delete link, method and route

--- TaskListRails/app/controllers/tasks_controller.rb
+++ TaskListRails/app/controllers/tasks_controller.rb
@@ -7,4 +7,29 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     render :show
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create #COMMON PATTERN FOR CREATE
+  @task = Task.new(task_create_params[:task])
+    if(@task.save)
+    redirect_to root_path#redirect in case user tries to post another form - brings them to another view
+    else
+    render :new
+    end
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def task_create_params#the params we want when we create a task, to sanitize and control user input
+    params.permit(task: [:name, :description])#these are the only parameters that can be passed from user.
+  end
 end
--- TaskListRails/config/routes.rb
+++ TaskListRails/config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
 root 'tasks#index'
 get '/' => 'tasks#index'
-get '/:id' => 'tasks#show'
+get '/tasks/new' => 'tasks#new'
+post '/tasks' => 'tasks#create', as: 'tasks'
+delete '/tasks/:id' => 'tasks#delete'
+get 'tasks/:id' => 'tasks#show'
+# resources :tasks
 # get '/:name' => 'tasks#by_name'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
