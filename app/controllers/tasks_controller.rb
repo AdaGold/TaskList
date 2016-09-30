4de8886b59460dfed9225db05e3f2e46cbe6d05f
jm-rives <joanna.rives@gmail.com>
2016-09-30 10:29:00 -0700

    ADD new gemfile

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,49 @@
 class TasksController < ApplicationController
+
   def index
+    @tasks = Task.all
   end
 
+  #opens new form for user submits to create
   def new
+    
+    @task = Task.new
+
   end
 
   def create
+    self.new
+    @params = params #parameters submitted from form (a hash) which is automated by rails
+    @task.title = params[:title]
+    @task.description = params[:description]
+    @task.completed_at = params[:completed_at]
+    @task.created_at = params[:created_at]
+    @task.updated_at = params[:updated_at]
+    @task.save
+  end
+  
+  #opens new form for user submits to update
+
+  def edit  
+    self.show
   end
 
-  def edit
+  def update
+
+    self.show
+      @task.title = params[:title]
+      @task.description = params[:description]
+      @task.completed_at = params[:completed_at]
+      @task.created_at = params[:created_at]
+      @task.updated_at = params[:updated_at]
   end
 
   def delete
+    num = params[:id].to_i
+    Post.find(num).destroy
   end
 
   def show
+    @task = Task.find(params[:id].to_i)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  # these are solely string data that rails seeks a matches for
+  # goal is human readablity
 
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/edit'
-
-  get 'tasks/delete'
+  root to: 'tasks#index'
+  # root does not need a hash rocket!
+  # <- this replaces  this get '/' # this is my index
+  get 'tasks/index', as: 'index'
 
   get 'tasks/show'
+  get 'tasks/show/:id' => 'tasks#show', as: 'show'
+
+  put 'post/create' => 'post#create', as: 'create'
+
+  get 'tasks/new' => 'tasks#new', as: 'new'
+
+  get 'tasks/:id/edit' => 'tasks#edit', as: 'edit'
+
+  patch 'tasks/:id/completed_at' => 'tasks#completed_at', as: 'completed_at'
+  
+  delete 'tasks/:id/destroy' => 'tasks#destroy', as: 'delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# do not put 'tasks/' before the action
