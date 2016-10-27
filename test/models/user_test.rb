f4ac91d938dcb68bc9a3b2a1f1df372df5a85a22
Leah Bueing <lbueing@gmail.com>
2016-10-26 21:35:25 -0700

    test complete for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+	test "test validity of user if email, uid, and provider not present" do
+	  user = User.new
+	  assert_not user.valid?
+	  user.email = "joe@schmoe.com"
+	  assert_not user.valid?
+	  user.uid = 12
+	  assert_not user.valid?
+	  user.provider = "github"
+	  assert user.valid?
+	end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'minitest-reporters'
+# require 'minitest-reporters'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -22,5 +22,5 @@ class ActiveSupport::TestCase
       provider: 'github', uid: '123545', info: { email: "a@b.com", name: "Ada" }
     })
   end
-  
+
 end
