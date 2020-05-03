9deb1ad90b9692a43b5be0f7ace8305cbcfa6cc8
dnguye2 <dnguyen1093@gmail.com>
2020-05-02 17:38:09 -0700

    controller index action shows tasks from database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,6 @@
 class TasksController < ApplicationController
-  TASKS = [
-    { name: "W1 Rails Lectures", description: "Watch lectures 2x the speed."},
-    { name: "Meal prep", description: "Cooking meals so I can run and code well."},
-    { name: "Decompress", description: "Spend time away from computer screen."}
-  ]
-
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
