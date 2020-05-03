c1e69e667f2c7f47b27f8f1a082ba81da8687891
dnguye2 <dnguyen1093@gmail.com>
2020-05-02 17:18:04 -0700

    controller action made for index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+  TASKS = [
+    { name: "W1 Rails Lectures", description: "Watch lectures 2x the speed."},
+    { name: "Meal prep", description: "Cooking meals so I can run and code well."},
+    { name: "Decompress", description: "Spend time away from computer screen."}
+  ]
+
+  def index
+    @tasks = TASKS
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # route for task index page
+  get '/tasks', to: 'tasks#index', as: 'tasks_path'
+
+  # show all tasks
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
 end
