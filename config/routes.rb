a99a49bbff3ebfabc7faabb1b5294cd19de7aa28
dnguye2 <dnguyen1093@gmail.com>
2020-05-02 17:59:03 -0700

    "can get index and root path" tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # root path
+  root :to => 'tasks#index'
+
   # route for task index page
-  get '/tasks', to: 'tasks#index', as: 'tasks_path'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
 
   # show all tasks
   get '/tasks/:id', to: 'tasks#show', as: 'task'
