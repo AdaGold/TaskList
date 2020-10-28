3770defc0cafa295b661d0ff892a002e05ab9583
Ana Parada <anakp07@gmail.com>
2020-10-28 12:56:30 -0700

    updated the task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 
-TASKS = [
-    { name: "Do Laundry", description: "Put dirty laundry into the washer", completed_at: "10/27/2020"},
-    { name: "Grocery shopping", description: "Go to Freddies to get groceries items", completed_at:"10/31/2020"}
-]
+TASKS = [ "Do laundry", "Grocery Shopping", "Go to the gym"]
+
 
 class TasksController < ApplicationController
   def index
     @tasks = TASKS
+    @tasks = Task.all
   end
 end
