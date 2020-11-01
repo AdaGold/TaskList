a758b0c620c746771aa980186291acfcff4519a3
dnsrocha <drocha@gmail.com>
2020-11-01 12:33:39 -0800

    wave 2.1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < ApplicationController
   def index
-    @tasks = []
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
+    if @task.nil?
+      redirect_to tasks_path
+      return
+    end
   end
 end
