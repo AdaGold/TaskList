5177e7103a41ca55005431563a81ef081c957b2f
dnsrocha <drocha@gmail.com>
2020-11-01 17:31:38 -0800

    fixed find_by method, completed Wave 2 - all tests passing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,10 +5,33 @@ class TasksController < ApplicationController
 
   def show
     task_id = params[:id].to_i
-    @task = TASKS[task_id]
+    @task = Task.find_by(id: task_id)
+
     if @task.nil?
       redirect_to tasks_path
       return
     end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task.id)
+      return
+    else
+      render :new, :bad_request # show the new task form view again
+      return
+    end
+  end
+
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:name, :description, :completed_at)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index'
   root to: "tasks#index"
 
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  patch 'tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'task_complete'
+
 end
